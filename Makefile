8b4533ee6b7c517e313a65dea0fdfe9edcbfd202
Sander Demeester <demeester.sander@gmail.com>
2012-12-27 20:03:28 +0100

    added a working clean target in makefile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ LD=ld -g -m elf_i386
 ASM=nasm -f elf -g
 
 selectionsort: selectionsort.o
-	$(LD) selectionsort/selectionsort.o -o selectionsort
+	$(LD) selectionsort/selectionsort.o -o selectionsort_bin
 selectionsort.o: 
 	$(ASM) -l selectionsort.lst selectionsort/selectionsort.asm
 
@@ -10,5 +10,10 @@ bubblesort: bubblesort.o
 	$(LD) bubblesort/bubblesort.o -o bubblesort_bin
 bubblesort.o: 
 	$(ASM) -l bubblesort.lst bubblesort/bubblesort.asm
+
+clean:
+	$(shell for file in `find . -name *.o` ;do rm $$file; done)
+	rm *.lst 2>&1 > /dev/null
+	rm *_bin 2>&1 > /dev/null
 
 
--- bubblesort/bubblesort.asm
+++ bubblesort/bubblesort.asm
@@ -3,16 +3,48 @@ filename	db 'nummers.txt' ; just use lenth of string
 filename_len	equ $-filename	 ; here we use a constant
 	
 section .bss	
-number_of_ent resb 4		; reverse 4bytes
+number_of_ent resb 4		; reverse 4bytes.
+buf	      resb 4		; reverse 1 byte for printing.
 	
 section .text
 	global _start	
 _start:
-	mov eax,42		
-	mov dword [number_of_ent],eax
+
+	;; read first byte from file to know how many elements there are
+	mov eax,5		; syscall open
+	mov ebx,filename	; filename
+	mov ecx,0		; read-only
+	int 80h			; call kernel
+
+	mov eax,3		; syscall read
+	mov ebx,eax		; file descriptor
+	mov ecx,number_of_ent	; location for storing our first 4 bytes
+	mov edx,4		; read 4 bytes
+	int 80h			; call the kernel
+	push dword [number_of_ent]
+	call print_byte
+	call ret		; lets go home
+
+ret:	
 	mov eax,1
-	mov ebx,[number_of_ent]
+	mov ebx,1
 	int 80h
+print_byte:
+	push ebp
+	mov ebp,esp
+
+	mov eax,[ebp+8]		; byte to print
+	mov dword [buf],eax
+	mov eax,4
+	mov ebx,1
+	mov ecx,buf
+	mov edx,1
+	int 80h
+	
+	mov esp,ebp
+	pop ebp
+	ret
+	
 	
 
 
