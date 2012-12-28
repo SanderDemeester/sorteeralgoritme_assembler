9dd797476194ec3b68ccaee7f5d3e4d35f2a78e3
Sander Demeester <demeester.sander@gmail.com>
2012-12-28 22:12:02 +0100

    nog wat spelen met assembly

--- bubblesort/bubblesort.asm
+++ bubblesort/bubblesort.asm
@@ -1,8 +1,8 @@
-%macro write 3
+%macro write 2
 	mov eax,4		; write syscall
 	mov ebx,STDOUT		; stdout
-	mov ecx,%2		; number of bytes	
-	mov edx,%3		; buffer
+	mov edx,%2		; number of bytes
+	mov ecx,%1		; buffer
 	int 80h			; call kernel
 %endmacro
 	
@@ -12,7 +12,7 @@ filename_len	equ $-filename	 ; here we use a constant
 STDOUT		equ	1	 ; stdout
 	
 section .bss	
-number_of_ent resb 4		; reverse 4bytes.
+number_of_ent resb 1		; reverse 1 byte
 buf	      resb 4		; reverse 1 byte for printing.
 	
 section .text
@@ -27,31 +27,18 @@ _start:
 
 	mov eax,3		; syscall read
 	mov ebx,eax		; file descriptor
-	mov ecx,number_of_ent	; location for storing our first 4 bytes
-	mov edx,4		; read 4 bytes
+	mov ecx,number_of_ent	; location for storing 1 byte
+	mov edx,1		; read 1 byte
 	int 80h			; call the kernel
-	push dword [number_of_ent]
-	call print_byte
+	mov eax,[number_of_ent]
+	write number_of_ent, 1
 	call ret		; lets go home
 
 ret:	
 	mov eax,1
 	mov ebx,1
 	int 80h
-print_byte:
-	push ebp
-	mov ebp,esp
-	mov eax,[ebp+8]		; byte to print
-	mov dword [buf],eax
-	mov eax,4
-	mov ebx,1
-	mov ecx,buf
-	mov edx,1
-	int 80h
-	
-	mov esp,ebp
-	pop ebp
-	ret
+
 	
 	
 
