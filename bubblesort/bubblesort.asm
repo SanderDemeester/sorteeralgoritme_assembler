813e87e188749351aff1ca6ad005fcfc89bb0919
Sander Demeester <demeester.sander@gmail.com>
2012-12-30 23:00:24 +0100

    fixed stupid error :)

--- bubblesort/bubblesort.asm
+++ bubblesort/bubblesort.asm
@@ -7,13 +7,12 @@
 %endmacro
 	
 section .data	
-filename	db 'nummers.txt' ; just use lenth of string
+filename	db 'nummers.txt',0 ; just use lenth of string
 filename_len	equ $-filename	 ; here we use a constant
 STDOUT		equ	1	 ; stdout
+buffer 		db 0
 	
-section .bss	
-number_of_ent resb 1		; reverse 1 byte
-buf	      resb 4		; reverse 1 byte for printing.
+section .bss
 	
 section .text
 	global _start	
@@ -24,17 +23,26 @@ _start:
 	mov ebx,filename	; filename
 	mov ecx,0		; read-only
 	int 80h			; call kernel
+	test eax,eax
+	js ret
 
+	mov edx,eax
+	pushad
 	mov eax,3		; syscall read
-	mov ebx,eax		; file descriptor
-	mov ecx,number_of_ent	; location for storing 1 byte
-	mov edx,1		; read 1 byte
+	mov ebx,edx		; file descriptor
+	mov ecx,buffer	    	; location for storing 4 bytes
+	mov edx,1		; read 4 bytes
 	int 80h			; call the kernel
-	mov eax,[number_of_ent]
-	write number_of_ent, 1
+	popad
+
+	mov eax,4
+	mov ebx,STDOUT
+	mov ecx,buffer
+	mov edx,1
+	int 80h
 	call ret		; lets go home
 
-ret:	
+ret:
 	mov eax,1
 	mov ebx,1
 	int 80h
