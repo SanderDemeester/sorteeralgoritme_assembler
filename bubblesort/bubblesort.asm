7534d8538d67d3a39adee1223ca5aee5140f2183
Sander Demeester <demeester.sander@gmail.com>
2012-12-31 18:47:11 +0100

    added 2 loops to start the bubblesort algoritme

--- bubblesort/bubblesort.asm
+++ bubblesort/bubblesort.asm
@@ -5,13 +5,15 @@
 	mov ecx,%1		; buffer
 	int 80h			; call kernel
 %endmacro
-	
+
 section .data	
-filename	db 'nummers.txt',0 ; just use lenth of string
-filename_len	equ $-filename	 ; here we use a constant
-STDOUT		equ	1	 ; stdout
-buffer 		dd 0
-	
+	filename	db 	'nummers.txt',0 	; just use lenth of string
+	filename_len	equ 	$-filename	   	; here we use a constant
+	STDOUT		equ	1	   		; stdout
+	buffer 		dd 	0		   	; buffer
+	nel		dd	0			; save the nummer of elements
+	begin_heap	dd	0			; begin heap
+	heap_pointer	dd	0			; heap pointer	
 section .bss
 	
 section .text
@@ -33,8 +35,44 @@ _start:
 	mov edx,4		; read 4 bytes
 	int 80h			; call the kernel
 	mov eax,[buffer]
+	mov [nel],eax
+	push ebx		; save file descr
+	
+	mov eax,45		; syscall 45
+	mov ebx,0		; argument
+	int 80h			; call kernel
 
-	write buffer,4
+	mov dword [begin_heap],eax ; end of heap to [begin_of_heap]
+	push eax		   ; save eax
+	mov eax,4
+	mul dword [buffer]	   ; number of bytes we need on the heap
+	mov [buffer],eax
+	pop eax
+	add eax,[buffer]
+	mov ebx,eax
+	mov eax,45
+	int 80h
+
+	pop ebx			; get back our file descriptor
+	push eax		; save heap breakpoint
+
+	mov eax,3		; syscall read
+	mov ecx,[begin_heap]		; upper bound of our heap
+	mov edx,[buffer]	; number of bytes we should read
+	int 80h			; 	
+
+
+	xor ecx,ecx		; first counter
+	xor edx,edx		; second counter
+
+.loop1:
+
+	inc ecx
+.loop2:	
+	cmp [nel],ecx
+	jne .loop1
+	
+xo	write buffer,4
 	call ret		; lets go home.
 
 ret:
